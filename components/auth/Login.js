e4c955951b22b9ef03b497e6f5e431b8831e4826
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-01-06 15:01:54 +0300

    ui enhancement and bug fixes

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
           <form className="shadow-lg" onSubmit={submitHandler}>
             <h1 className="mb-3">Login</h1>
             <div className="form-group">
-              <label for="email_field">Email</label>
+              <label htmlFor="email_field">Email</label>
               <input
                 type="email"
                 id="email_field"
@@ -50,7 +50,7 @@ const Login = () => {
             </div>
 
             <div className="form-group">
-              <label for="password_field">Password</label>
+              <label htmlFor="password_field">Password</label>
               <input
                 type="password"
                 id="password_field"
